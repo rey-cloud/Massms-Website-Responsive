bb20481d29d6389cfacc366185b3565b508a76d0
Reynaldo A. Baja Jr <reynaldobaja2@gmail.com>
2023-04-15 14:25:33 +0800

    my-update

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ['Montserrat'],
+      },
+      width: {
+        'w-paragraph': '580px',
+        '550':'550px'
+      }
+    },
+  },
+  plugins: [],
+}
+
